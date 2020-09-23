ec11c9cb5acb434c66b4b3f19039f0f459644703
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-23 16:07:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,8 @@ def join_nested_strings(src)
      nested_index = 0
      string = "string".class
      while nested_index < src[array_index].length do
-       if src[array_index][nested_index].class == string
+      #  if src[array_index][nested_index].class == string
+       if src[array_index][nested_index].is_a? String
          new_string << src[array_index][nested_index] + " "
        end
         nested_index += 1
